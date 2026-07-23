ec5cafb49770fa9d5f14fc1d2801886d4da1eb2c
spaceyounoob <jeevacation2331@gmail.com>
2026-07-23 22:14:17 +0800

    a

--- src/Shell/Explorer/StartButton.cpp
+++ src/Shell/Explorer/StartButton.cpp
@@ -1234,7 +1234,8 @@ bool CStartButton::_OnThemeChanged(bool bForceUpdate)
     }
 
     bool bThemeApplied = false;
-    _hTheme = OpenThemeData(_hwndStart, L"Button");
+ //   _hTheme = OpenThemeData(_hwndStart, L"Button");
+      HBITMAP hbmStartButton = (HBITMAP)LoadImageA(NULL, "resources/orb.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE); // Loads start orb --space
 
     if (_hTheme)
     {
